7c9db005faae03651ff0c040a666dd3aa728b6b7
DorHaimovich12 <Dor.haimovich@stu.shenkar.ac.il>
2024-08-31 17:14:04 +0300

    refactor/remove-console-log

--- src/requests.js
+++ src/requests.js
@@ -17,18 +17,12 @@ const sendSurveyResult = async (exp_id, user_id, opinion, surveyNumber) => {
   let URL;
   if (surveyNumber === 1) URL = baseURL + `surveys/pre`;
   else URL = baseURL + `surveys/post`;
-  console.log({
-    exp_id,
-    user_id,
-    opinion,
-  });
   try {
     const response = await axios.post(URL, {
       exp_id,
       user_id,
       opinion,
     });
-    console.log("response ", response.data);
     return response.data;
   } catch (error) {
     console.error("Failed to send survey result", error);
@@ -37,12 +31,9 @@ const sendSurveyResult = async (exp_id, user_id, opinion, surveyNumber) => {
 };
 
 const isAnsweredFirstSurvey = async (exp_id, user_id) => {
-  console.log({ exp_id, user_id });
   const URL = baseURL + `surveys/pre/is-answer`;
   try {
     const response = await axios.post(URL, { exp_id, user_id });
-
-    console.log(response.data[0].is_pre);
     return response.data[0].is_pre;
   } catch (error) {
     console.error("Failed to fetch experiment", error);
@@ -66,21 +57,17 @@ const saveParticipant = async (exp_id, user_id) => {
   try {
     axios.post(URL, { exp_id, user_id });
   } catch (error) {
-    console.log("Failed to save participant", error);
     return null;
   }
 };
 
 const isParticipantConnected = async (exp_id, user_id) => {
   const URL = baseURL + `participant/${user_id}/experiment/${exp_id}/is-in`;
-  console.log({ exp_id, user_id });
-  console.log(URL);
   try {
     const response = await axios.get(URL);
-    console.log(response.data);
     return response.data[0].is_in;
   } catch (error) {
-    console.log("Failed to check participant connection", error);
+    console.error("Failed to check participant connection", error);
     return null;
   }
 };
